ba985fcb21e85affd12d8d5d1bd1d373fa24ff92
sidou907 <sidalighomrani@gmail.com>
2026-06-22 11:50:48 +0200

    Add files via upload

--- fiche.py
+++ fiche.py
@@ -3,13 +3,11 @@ import ssl
 
 def get_connection():
     context = ssl.create_default_context()
-    
-    # تم وضع البيانات الدقيقة جداً لحل مشكلة المصادقة
     conn = pg8000.dbapi.connect(
         host="ep-fragrant-water-alyos8cz-pooler.c-3.eu-central-1.aws.neon.tech",
         database="neondb",
         user="neondb_owner",
-        password="npg_cXHMGpT80QUt", # السر كان في الرقم صفر 0 هنا!
+        password="npg_cXHMGpT80QUt",
         port=5432,
         ssl_context=context
     )
